9a022139543b705f4eed49cbf8bc817ea33aea5b
Nick Chamberlain <nick.chamberlain.jr@gmail.com>
2017-09-01 17:12:30 -0400

    build working on net45

---
+++ src/Lighthouse/LighthouseHostFactory.cs
@@ -0,0 +1,66 @@
+﻿// Copyright 2014-2015 Aaron Stannard, Petabridge LLC
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+using System.Configuration;
+using System.Linq;
+using Akka.Actor;
+using Akka.Configuration;
+using Akka.Configuration.Hocon;
+using ConfigurationException = Akka.Configuration.ConfigurationException;
+
+namespace Lighthouse
+{
+    /// <summary>
+    /// Launcher for the Lighthouse <see cref="ActorSystem"/>
+    /// </summary>
+    public static class LighthouseHostFactory
+    {
+        public static ActorSystem LaunchLighthouse(string ipAddress = null, int? specifiedPort = null)
+        {
+            var systemName = "lighthouse";
+            var section = (AkkaConfigurationSection)ConfigurationManager.GetSection("akka");
+            var clusterConfig = section.AkkaConfig;
+
+            var lighthouseConfig = clusterConfig.GetConfig("lighthouse");
+            if (lighthouseConfig != null)
+            {
+                systemName = lighthouseConfig.GetString("actorsystem", systemName);
+            }
+
+            var remoteConfig = clusterConfig.GetConfig("akka.remote");
+            ipAddress = ipAddress ??
+                        remoteConfig.GetString("dot-netty.tcp.public-hostname") ??
+                        "127.0.0.1"; //localhost as a final default
+            int port = specifiedPort ?? remoteConfig.GetInt("dot-netty.tcp.port");
+
+            if(port == 0) throw new ConfigurationException("Need to specify an explicit port for Lighthouse. Found an undefined port or a port value of 0 in App.config.");
+
+            var selfAddress = string.Format("akka.tcp://{0}@{1}:{2}", systemName, ipAddress, port);
+            var seeds = clusterConfig.GetStringList("akka.cluster.seed-nodes");
+            if (!seeds.Contains(selfAddress))
+            {
+                seeds.Add(selfAddress);
+            }
+
+            var injectedClusterConfigString = seeds.Aggregate("akka.cluster.seed-nodes = [", (current, seed) => current + (@"""" + seed + @""", "));
+            injectedClusterConfigString += "]";
+
+            var finalConfig = ConfigurationFactory.ParseString(
+                string.Format(@"akka.remote.dot-netty.tcp.public-hostname = {0} 
+akka.remote.dot-netty.tcp.port = {1}", ipAddress, port))
+                .WithFallback(ConfigurationFactory.ParseString(injectedClusterConfigString))
+                .WithFallback(clusterConfig);
+
+            return ActorSystem.Create(systemName, finalConfig);
+        }
+    }
+}
---
+++ src/Lighthouse/Program.cs
@@ -0,0 +1,41 @@
+﻿// Copyright 2014-2015 Aaron Stannard, Petabridge LLC
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+using Topshelf;
+
+namespace Lighthouse
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            return (int) HostFactory.Run(x =>
+            {
+                x.Service<LighthouseService>(s =>
+                {
+                    s.ConstructUsing(ss => new LighthouseService());
+                    s.WhenStarted(ss => ss.Start());
+                    s.WhenStopped(ss => ss.StopAsync().Wait());
+                });
+
+                x.SetServiceName("Lighthouse");
+                x.SetDisplayName("Lighthouse Service Discovery");
+                x.SetDescription("Lighthouse Service Discovery for Akka.NET Clusters");
+
+                x.RunAsNetworkService();
+                x.StartAutomatically();
+                x.UseNLog();
+                x.EnableServiceRecovery(r => r.RestartService(1));
+            });
+        }
+    }
+}
