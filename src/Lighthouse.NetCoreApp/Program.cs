0d80ddb6b01d4b978f264f277cb94526df02316d
Nick Chamberlain <nick.chamberlain.jr@gmail.com>
2017-09-08 14:58:46 -0400

    lighthouse.netcoreapp

---
+++ src/Lighthouse.NetCoreApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Lighthouse.NetCoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lighthouseService = new LighthouseService();
+            lighthouseService.Start();
+            Console.ReadLine();
+            lighthouseService.StopAsync().Wait();
+        }
+    }
+}
