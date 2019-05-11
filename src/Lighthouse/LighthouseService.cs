474ead3a6edaa01c39302e333adb198f295618f7
Aaron Stannard <aaron@petabridge.com>
2019-05-11 18:42:02 -0500

    upgraded to Akka.NET 1.3.13, .NET Core 2.1, PBM 0.6.0

--- src/Lighthouse/LighthouseService.cs
+++ src/Lighthouse/LighthouseService.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2014-2015 Aaron Stannard, Petabridge LLC
+﻿// Copyright 2014-2019 Aaron Stannard, Petabridge LLC
 //  
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
 // this file except in compliance with the License. You may obtain a copy of the 
