f5a291f614196df4d0edd87200a802d594a313f2
Aaron Stannard <aaron@petabridge.com>
2022-08-10 08:00:55 -0500

    upgrade to .NET 6

--- src/Lighthouse/buildLinuxDockerImages.sh
+++ src/Lighthouse/buildLinuxDockerImages.sh
@@ -20,7 +20,7 @@ fi
 
 
 echo "Building project..."
-dotnet publish -c Release --framework netcoreapp3.1
+dotnet publish -c Release --framework net6.0
 dotnet build-server shutdown
 
 LINUX_IMAGE="$IMAGE_NAME:linux-$IMAGE_VERSION"
